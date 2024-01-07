2d6f8d2a3bfa5c675f32e9bdb4799246423e97cf
刘飞 <4189810126@qq.com>
2024-01-07 16:13:39 +0800

    提交

--- unpackage/dist/dev/mp-weixin/api/erp/material/material.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const utils_request = require("../../../utils/request.js");
-function getMaterialInfo(data) {
-  return utils_request.base({ url: "/erp/api/v1/material/getMaterialInfo", method: "GET", "data": data });
-}
-const materialApi = {
-  getMaterialInfo
-};
-exports.materialApi = materialApi;
