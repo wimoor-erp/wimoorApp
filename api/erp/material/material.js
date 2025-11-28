c20f225b71c87e6053459e5ea3b7a94ae23db9ff
Wimoor 万墨ERP <felix.liu.hn@gmail.com>
2025-11-28 12:59:28 +0800

    深圳市万墨信息科技有限公司
    删除旧版本

--- api/erp/material/material.js
+++
@@ -1,9 +0,0 @@
-
-import request from "@/utils/request";
- 
-function getMaterialInfo(data){
-	return request({url:"/erp/api/v1/material/getMaterialInfo",method:"GET","data":data})
-}
-export default {
-      getMaterialInfo
-	};
